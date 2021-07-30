3789f1363c13db9910f0d3d7b022b1db09b17ef9
Fernando Daniel Romano <fernandoreceptor2014@gmail.com>
2021-07-30 16:23:20 -0300

    Configurando las rutas y las vistas iniciales

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,36 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'home',
+    component: () => import(/* webpackChunkName: "Home" */ '@/views/Home.vue')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/registro',
+    name: 'registro',
+    redirect: 'registro/listado',
+    component: () => import(/* webpackChunkName: "RegisterContainer" */ '@/views/Register/RegisterContainer.vue'),
+    children:[
+      {
+        path: 'listado',
+        name: 'registro-listado',
+        component: () => import(/* webpackChunkName: "RegisterList" */ '@/views/Register/RegisterList.vue')
+      },
+      {
+        path: 'crear',
+        name: 'registro-crear',
+        component: () => import(/* webpackChunkName: "RegisterCreate" */ '@/views/Register/RegisterCreate.vue')
+      },
+      {
+        path: 'actualizar/:id',
+        name: 'registro-actualizar',
+        component: () => import(/* webpackChunkName: "RegisterUpdate" */ '@/views/Register/RegisterUpdate.vue')
+      },
+    ]
   }
 ]
 

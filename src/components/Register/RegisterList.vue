7799ac75f03dc04ac06c5ce33cf159424abc1c83
Fernando Daniel Romano <fernandoreceptor2014@gmail.com>
2021-07-30 21:14:05 -0300

    Generando los componentes para trabajar con la vista RegisterList

---
+++ src/components/Register/RegisterList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="mx-8">
+    <table v-if="registers.length" class="min-w-max w-full table-auto shadow-lg">
+      <thead>
+        <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+          <th class="py-3 px-6 text-left">Nombre</th>
+          <th class="py-3 px-6 text-left">Actividad</th>
+          <th class="py-3 px-6 text-center">Detalles</th>
+          <th class="py-3 px-6 text-center">Acciones</th>
+        </tr>
+      </thead>
+      <tbody class="text-gray-600 text-sm font-light">
+        <register-item 
+            v-for="register in registers" 
+            :key="register.id"
+            :register="register">
+        </register-item>
+      </tbody>
+    </table>
+
+    <div v-else class="py-4 border bg-white rounded-md shadow-md">
+        <h3 class="text-center font-medium">No existen registros creados aún</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+import RegisterItem from "./RegisterItem.vue";
+
+export default {
+  name: "RegisterList",
+  props: {
+    registers: {
+      type: Array,
+      required: true,
+    },
+  },
+  components: {
+    RegisterItem,
+  },
+};
+</script>
--- src/views/Register/RegisterList.vue
+++ src/views/Register/RegisterList.vue
@@ -1,11 +1,27 @@
 <template>
     <div>
-        Listado de registros
+        <h2 class="text-center text-3xl font-semibold mb-4">Listado de Registros</h2>
+        <list :registers="registers"></list>
     </div>
 </template>
 
 <script>
+    import List from '@/components/Register/RegisterList.vue'
+    import { mapState, mapActions } from 'vuex'
+
     export default {
-        name: 'RegisterList'
+        name: 'RegisterList',
+        async mounted(){
+            await this.getRegisters()
+        },
+        components:{
+            List
+        },
+        computed:{
+            ...mapState('register', ['registers']),
+        },
+        methods:{
+            ...mapActions('register', ['getRegisters'])
+        }
     }
 </script>
